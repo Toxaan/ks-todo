432d14302df365c87204eef639f6f567627639b0
anton_kuchkov <kuanton@mail.ru>
2020-02-26 20:24:42 +0300

    todo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,87 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Row, ListGroup, Container, Form, Button } from 'bootstrap-4-react';
 
-function App() {
+//import PropTypes from 'prop-types'
+
+function App() { 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="wrapper">
+      <Notes/>
+    </Container>
   );
+}
+
+
+class Notes extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      note: [
+        {name: "Встать"},
+        {name: "Умыться"},
+        {name: "Покушать"}
+      ],
+      strNote: ""
+    };
+  }
+
+  addNote(str){
+    if(str.trim() !== ""){
+      var oldState = this.state.note;
+      oldState.push({name: str})
+      this.setState({
+        note: oldState,
+        strNote: ""
+      })
+    }
+  }
+
+  updateStrNode(str){
+    this.setState({
+      strNote: str 
+    })
+  }
+
+  removeNote(id){
+    var newState = this.state.note.filter((note,index) => index !== id)
+    this.setState({
+      note: newState
+    })
+  }
+
+  render(){
+    return (
+      <div className="vse">
+        <Form>
+          <Form.Input 
+              type="text"
+              placeholder="Новая заметка"
+              value={this.state.strNote}
+              onChange={(event) => this.updateStrNode(event.target.value)}
+          />
+        </Form>
+        <Button className="btnAdd" success onClick={() => this.addNote(this.state.strNote)} >
+              Добавить заметку
+        </Button>
+        <ListGroup>
+          {this.state.note.map((note,index) => {
+            return (
+              <ListGroup.Item key={index}style={{backgroundColor: "rgb(246, 248, 134)"}}>
+                <Row justifyContent="between" key={index} style={{paddingLeft:"10px", paddingRight:"10px"}}>
+                  <span className="perenos">
+                    <strong>{index +1}</strong>
+                    &nbsp;
+                    {note.name}
+                  </span>
+                  <Button danger outline sm onClick={() => this.removeNote(index)}>x</Button>
+                </Row >
+              </ListGroup.Item>
+            )
+          })}
+        </ListGroup>
+      </div>
+    )
+  }
 }
 
 export default App;
