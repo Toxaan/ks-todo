047399835133b7028c5296d5e1e6bbd0d3f3d21a
anton_kuchkov <kuanton@mail.ru>
2020-02-26 20:14:53 +0300

    Updates

---
+++ precache-manifest.fbdc9f3f24bba72d52a68b9f78665d0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fdb173e11a36242944dfe0fc230df13",
+    "url": "/ks-todo/index.html"
+  },
+  {
+    "revision": "3a8a0ea810b751f59247",
+    "url": "/ks-todo/static/css/main.261b6a14.chunk.css"
+  },
+  {
+    "revision": "fa113d5e0d219d14baa1",
+    "url": "/ks-todo/static/js/2.d38128e6.chunk.js"
+  },
+  {
+    "revision": "74b769bf19f36aac454bdb654f5043d9",
+    "url": "/ks-todo/static/js/2.d38128e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a8a0ea810b751f59247",
+    "url": "/ks-todo/static/js/main.e3333704.chunk.js"
+  },
+  {
+    "revision": "7dac6eeabfe81d653a5d",
+    "url": "/ks-todo/static/js/runtime-main.3018a561.js"
+  }
+]);
